bd4f4e3f4a2dcb208b15923dc7f115d6cd1bcd36
umaircs01 <umaircs01@gmail.com>
2018-06-25 06:22:51 +0500

    2nd

---
+++ KhalidPetroleum/Controllers/MainController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhalidPetroleum.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+		public ActionResult SalePurchase()
+        {
+            return View();
+        }
+		public ActionResult Checklist()
+        {
+            return View();
+        }
+		public ActionResult Attendance()
+		{
+			return View();
+		}
+		public ActionResult DailyExpenses()
+        {
+            return View();
+        }
+		public ActionResult CashReceived()
+        {
+            return View();
+        }
+		public ActionResult Payments()
+        {
+            return View();
+        }
+    }
+}
